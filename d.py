445dd44c35450366b286d6139c7c81cd09aaa8e1
Davidemenike <59932349+Davidemenike@users.noreply.github.com>
2022-02-04 15:15:23 -0600

    Add files via upload

--- d.py
+++ d.py
@@ -1,17 +1,18 @@
+from ast import Continue
 import os
 
-var_1 = ['a','b','c','d']
+var_1 = ['a','b','c','d','e','f']
 
 var_2 = ['m','n','o']
 
 root = '/Magers'
 
-try:
 
-    for folders in var_1:
-        for nestedfolder in var_2:
-            os.makedirs(os.path.join(root,folders,nestedfolder))
-
-    
-except FileExistsError:
-    print("file already exists")
+for folders in var_1:
+    for nestedfolder in var_2:
+        pwd = os.path.join(root,folders,nestedfolder)
+        print(pwd)
+        if os.path.isdir(pwd) == True:
+            Continue
+        else: 
+            os.makedirs(pwd)
