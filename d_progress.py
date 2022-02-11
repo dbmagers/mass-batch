6fd2ecffccfd31d80be21fa3b6f3092cab52085e
Davidemenike <59932349+Davidemenike@users.noreply.github.com>
2022-02-11 15:24:40 -0600

    Add files via upload

--- d_progress.py
+++ d_progress.py
@@ -1,6 +1,27 @@
 import os
+import yaml
+import argparse
+
+
+#use yaml to sort our folders
+#implement arc parse
+
+var1_yaml = """
+- 'a'
+' 'b'
+- 'c'
+- 'd'
+"""
+
+var = yaml.safe_load(var1_yaml)
 
 var_1 = ['a','b','c','d']
+
+var1_yam2 = """
+- 'm'
+' 'n'
+- 'o'
+"""
 
 var_2 = ['m','n','o']
 
@@ -11,9 +32,10 @@ try:
 
     for folders in var_1:
         for nestedfolder in var_2:
-            os.makedirs(os.path.join(root,folders,nestedfolder))
+            pwd =(os.path.join(root,folders,nestedfolder))
+            os.makedirs(pwd)
             file_name = "test.txt"
-            file1 = open(nestedfolder, "w")
+            file1 = open(pwd+"/input.dat", "w")
             file1.write("Chemistry and star stuffs?")
             file1.close()
     
