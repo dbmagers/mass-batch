36fbbba75f8757351974ae908a4a2108f89799cd
Chiemela Emenike <chiemelaemenike@students.belhaven.edu>
2022-03-18 15:34:00 -0500

    finally_got

---
+++ d_progress.py
@@ -0,0 +1,52 @@
+import os
+import yaml
+import argparse
+from mako.template import Template
+
+
+
+# 1. understand mako libray, find examples
+# put txt files with same format but different values in the bottom folder of Magers
+
+
+
+#use yaml to sort our folders
+#implement arc parse
+
+yaml_string = """
+- [{'a':'e'},{'b':'f'}]
+- [{'m':'r'},{'n':'s'}]
+"""
+
+#stream = open('data.yaml','r')
+#yamldata = yaml.safe_load(stream)
+#stream.close()
+yamldata = yaml.safe_load(yaml_string)
+
+#print(yamldata)
+
+var_1 = ['a','b','c','d']
+var_2 = ['m','n','o']
+
+root = 'Magers'
+
+dict = {'a':'e'}
+
+
+
+#last check, for a given dict key, give us the value associated
+print(dict.items())
+#print(yamldata[0][0].keys()[0])
+
+try:
+    for level1 in yamldata:
+        for level2 in yamldata[level1]:
+            pwd =(os.path.join(root,level1,level2.keys()))
+            os.makedirs(pwd)
+            mytemplate = Template(filenmae = 'templates/test.txt')
+            file1 = open(pwd+"/input.dat", "w")
+            file1.write("Chemistry and star stuffs?")
+            file1.close()
+    
+except FileExistsError:
+    print("file already exists")
